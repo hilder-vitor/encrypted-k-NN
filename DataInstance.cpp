34ef2b9b08c723b5f3f297426be1a7cd3b74e305
vitorlima <hilder.vitor@gmail.com>
2016-06-01 14:29:37 -0300

    Created class to represent a data instance

---
+++ DataInstance.cpp
@@ -0,0 +1,53 @@
+#include "DataInstance.h"
+
+using namespace std;
+
+DataInstance::DataInstance(int _id, const std::vector<int>& _data, int _class) : id(_id), data(_data), label(_class), distance(-1){
+}
+
+void DataInstance::set_distance(const DataInstance& query){
+	distance = 0;
+	unsigned int P = data.size();
+	for (unsigned int i = 0; i < P; i++){
+		distance += (query[i] - data[i]) * (query[i] - data[i]);
+	}
+}
+
+int DataInstance::get_distance() const {
+	return distance;
+}
+
+int DataInstance::get_id() const{
+	return id;
+}
+
+int DataInstance::get_class() const{
+	return label;
+}
+
+int DataInstance::size() const{
+	return data.size();
+}
+
+bool DataInstance::operator<(const DataInstance& other) const {
+	return distance < other.get_distance();
+}
+	
+
+const int& DataInstance::operator[](const int& i) const{
+	return data[i];
+}
+
+std::ostream& operator<<(std::ostream& os, const DataInstance& v){
+	os << v.get_id() << " --> [";
+	unsigned int P = v.size();
+	if (0 == P){
+		os << "]" << endl;
+	}else{
+		for (unsigned int i = 0; i < P - 1; i++){
+			os << v[i] << ", ";
+		}
+		os << v[P - 1] << "]" << endl;
+	}
+	return os;
+}
---
+++ DataInstance.h
@@ -0,0 +1,32 @@
+#ifndef __HEKNN_DATA_INSTANCE_
+#define __HEKNN_DATA_INSTANCE_
+
+#include <vector>
+#include <iostream>
+
+class DataInstance {
+    private:
+	std::vector<int> data;
+	int id;
+	int label; // the class assigned to this data_instance
+	int distance; // the distance to the query vector
+    
+    public:
+
+	DataInstance(int id, const std::vector<int>& data, int _class);
+
+	void set_distance(const DataInstance& query);
+	int get_distance() const;
+
+	int get_id() const;
+	int get_class() const;
+
+	int size() const;
+
+	bool operator<(const DataInstance&) const;
+	
+	const int& operator[](const int&) const;
+};	
+
+std::ostream& operator<<(std::ostream&, const DataInstance&);
+#endif
