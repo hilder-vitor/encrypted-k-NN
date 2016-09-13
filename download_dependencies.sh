84e0f77f24895f19e8b1f1da65e8d293664a0dd2
vitorlima <hilder.vitor@gmail.com>
2016-09-13 15:11:15 -0300

    Improved script to download dependencies

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+if [ -d lib ]; then
+	rm -rf lib
+fi
+
+mkdir /lib
+
+
 ###  YASHE LIB
 rm -rf lib/yashe
-wget https://github.com/hilder-vitor/yashe/archive/master.zip
-unzip master.zip
+wget https://github.com/hilder-vitor/yashe/archive/master.tar.gz
+tar xf master.tar.gz
 mkdir -p lib/yashe
 mv yashe-master/* lib/yashe/
 rmdir yashe-master/
-rm master.zip
+rm master.tar.gz
 
 
 ### OPE LIB
 wget https://github.com/hilder-vitor/ope-from-cryptodb/archive/master.zip
 unzip master.zip
-mv ope-from-cryptodb-master/ lib/ope/
+mkdir lib/ope
+mv ope-from-cryptodb-master/* lib/ope/
+rmdir ope-from-cryptodb-master/
+rm master.zip
+
+### PAILLIER LIB
+wget https://github.com/hilder-vitor/paillier/archive/master.zip
+unzip master.zip
+mkdir lib/paillier
+mv paillier-master/* lib/paillier
+rmdir paillier-master/
 rm master.zip
 
