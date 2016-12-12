92ffd67e693ba424a003ee5d655967ec0b40537a
Vitor Lima <vitor_lp@yahoo.com>
2016-12-12 09:41:43 -0200

    Now also showing (python's knn) relative accuracy

--- KnnClient.cpp
+++ KnnClient.cpp
@@ -37,9 +37,12 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
+	// example format: datasets/abalone/abalone.data
 	string dataset_name = argv[1];
 	// number of neighbours in the k-NN
 	unsigned int k = stoi(argv[2]);
+
+	ifstream python_predicted_classes(dataset_name + ".prediction" + ".k" + to_string(k), ifstream::in);
 
 	// plaintext range's length in bits (plaintexts are in [0, 2**P[
 	unsigned int P = 32;
@@ -56,18 +59,24 @@ int main(int argc, char **argv) {
 
 	Dataset data(dataset_name);
 	cout << data << endl;
-	cout << "# classes: " << data.number_of_classes << endl;
-	vector<int> hits_by_class(data.number_of_classes + 1, 0);
-	vector<int> errors_by_class(data.number_of_classes + 1, 0);
+	//cout << "# classes: " << data.number_of_classes << endl;
+	//vector<int> hits_by_class(data.number_of_classes + 1, 0);
+	//vector<int> errors_by_class(data.number_of_classes + 1, 0);
 
 	timing timing;
 	EncryptedDataset enc_data(data, ope, paillier);
 
 	HomomorphicKnn knn(k, enc_data.training_data, paillier);
 
-	unsigned int right = 0;
-	unsigned int wrong = 0;
+	unsigned int right_expected = 0; // number of times the class in the original dataset file is assigned correctly
+	unsigned int wrong_expected = 0;
+
+	unsigned int right_predicted = 0; // number of times the class predicted by python's knn is assigned
+	unsigned int wrong_predicted = 0;
+
+
 	unsigned int total_test_cases = enc_data.testing_data.size();
+	unsigned int class_predicted_by_knn_python = 666;
 
 	for (unsigned int j = 0; j < total_test_cases; j++){
 		timing.start();
@@ -75,29 +84,31 @@ int main(int argc, char **argv) {
 		timing.stop("time homomorphic classification");
 		unsigned int assigned_class = decrypt_assigned_class(encrypted_class, paillier, data.number_of_classes, gap, two_to_gap);
 		unsigned int expected_class = data.testing_data[j].get_class();
+		python_predicted_classes >> class_predicted_by_knn_python;
+		
 		cout << "instance #"<< j << ": assigned class: " << assigned_class << endl; 
 		cout << "instance #"<< j << ": expected class: " << expected_class << endl;
+		cout << "instance #"<< j << ": predicted class: " << class_predicted_by_knn_python << endl;
 		cout << endl;
 		if (assigned_class == expected_class){
-			right++;
-			hits_by_class[expected_class]++;
+			right_expected++;
 		}else{
-			wrong++;
-			errors_by_class[expected_class]++;
+			wrong_expected++;
+		}
+		if (assigned_class == class_predicted_by_knn_python){
+			right_predicted++;
+		}else{
+			wrong_predicted++;
 		}
 	}
+
+	python_predicted_classes.close();
+
+
 //	timing_whole_classification.stop("Time to the whole classification");
 	timing.show();
-	cout << "accuracy: " << ((double) right) / total_test_cases << endl;
-
-	for (unsigned int i = 1; i < hits_by_class.size(); i++){
-		unsigned int total_per_class = hits_by_class[i] + errors_by_class[i];
-		if (total_per_class == 0)
-			cout << "  class " << i << ":  ---" << endl;
-		else
-			cout << "  class " << i << ":";
-			cout << " hits: " << hits_by_class[i] << "; errors: " << errors_by_class[i] << "; accuracy: " << ((double) hits_by_class[i]) / total_per_class << endl;
-	}
+	cout << "accuracy: " << ((double) right_expected) / total_test_cases << endl;
+	cout << "(python's knn relative) accuracy: " << ((double) right_predicted) / total_test_cases << endl;
 
 	return 0;
 }
